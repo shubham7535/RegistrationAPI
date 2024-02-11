c1324451d266ebb8b49098d45551e152a54bf203
Shubham Mamale <shubhammamale75@gmail.com>
2024-02-11 22:19:02 +0530

    api committed

---
+++ RegistrationApi/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using RegistrationApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistrationApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("AllowOrigin")]
+    public class UserController : ControllerBase
+    {
+
+        private readonly IConfiguration _config;
+        public readonly UserContext _context;
+
+        public UserController(IConfiguration config, UserContext context)
+        {
+            _config = config;
+            _context = context;
+        }
+
+        [HttpPost("CreateUser")]
+        public IActionResult CrateUser(User user)
+        {
+            user.MemberSince = DateTime.Now;
+            _context.Users.Add(user);
+            _context.SaveChanges();
+            return Ok("Success");
+        }
+    }
+}
